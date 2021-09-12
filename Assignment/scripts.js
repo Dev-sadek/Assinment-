5387bd7519cc8d9b443500932e69ee8a4cb26604
Sheak Sadek <90350634+Sadek68@users.noreply.github.com>
2021-09-12 22:24:27 +0600

    Add files via upload

---
+++ Assignment/scripts.js
@@ -0,0 +1,6 @@
+function changtext(){
+document.getElementById('labaid').innerHTML='Labaid Group started rolling with the inception of Labaid Ltd in 1989. Since, it marches on and on, blossomed into one of the fastest growing groups of companies mainly in health care services but also in other fields like real state, agro farming, financial sector and education.';
+document.getElementById('labaid').style.color='red';
+document.getElementById('labaid').style.fontSize='20px';
+Document.getElementById('labaid').style.backgroundColor='white';
+}
